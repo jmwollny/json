c38c3aa9f243372f22d5b85d66a23d792f4bf666
Jonathan Wollny <jon.wollny@uk.ibm.com>
2017-10-10 16:43:48 +0100

    Ignore any exceptions thrown by the JSON parser

--- content.js
+++ content.js
@@ -4,54 +4,53 @@ var prettyPrintedJson;
 var pre;
 var code;
 
-if (!library)
-{
-   var library = {};
+if (!library) {
+    var library = {};
 }
 
 // http://jsfiddle.net/unlsj/
 library.json = {
-   replacer: function(match, pIndent, pKey, pVal, pEnd) {
-	  //var key = '<span class=json-key';
-	  var key = '<span class=';
-	  var val = '<span class=json-value>';
-	  var str = '<span class=json-string>';
-	  var r = pIndent || '';
+    replacer: function(match, pIndent, pKey, pVal, pEnd) {
+        //var key = '<span class=json-key';
+        var key = '<span class=';
+        var val = '<span class=json-value>';
+        var str = '<span class=json-string>';
+        var r = pIndent || '';
 
-	  if (pKey)
-	  {
-		 var propName = pKey.replace(/[": ]/g, '');
-		 if ( propName === "name" )
-			 key += " json-name";
-		 else if ( propName === "id" )
-			 key += " json-id";
-		 key += ">";
-		 r = r + key + propName + '</span>: ';
-	  }
-	  if (pVal)
-		 r = r + (pVal[0] == '"' ? str : val) + pVal + '</span>';
-	  return r + (pEnd || '');
-	  },
-   prettyPrint: function(obj) {
-	  var jsonLine = /^( *)("[\w]+": )?("[^"]*"|[\w.+-]*)?([,[{])?$/mg;
-	  return JSON.stringify(obj, null, 3)
-		 .replace(/&/g, '&amp;').replace(/\\"/g, '&quot;')
-		 .replace(/</g, '&lt;').replace(/>/g, '&gt;')
-		 .replace(jsonLine, library.json.replacer);
-	  }
-   };
-   
-jsonObject = JSON.parse( document.body.innerText );
+        if (pKey) {
+            var propName = pKey.replace(/[": ]/g, '');
+            if (propName === "name")
+                key += " json-name";
+            else if (propName === "id")
+                key += " json-id";
+            key += ">";
+            r = r + key + propName + '</span>: ';
+        }
+        if (pVal)
+            r = r + (pVal[0] == '"'
+                ? str
+                : val) + pVal + '</span>';
+        return r + (pEnd || '');
+    },
+    prettyPrint: function(obj) {
+        var jsonLine = /^( *)("[\w]+": )?("[^"]*"|[\w.+-]*)?([,[{])?$/mg;
+        return JSON.stringify(obj, null, 3).replace(/&/g, '&amp;').replace(/\\"/g, '&quot;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(jsonLine, library.json.replacer);
+    }
+};
 
-if ( jsonObject )
-{
-	var prettyPrintedJson = library.json.prettyPrint( jsonObject );
-	var pre = document.createElement("pre");
-	var code = document.createElement("code")
+try {
+    jsonObject = JSON.parse(document.body.innerText);
+    if (jsonObject) {
+        var prettyPrintedJson = library.json.prettyPrint(jsonObject);
+        var pre = document.createElement("pre");
+        var code = document.createElement("code")
 
-	pre.appendChild(code);
-	document.body.firstChild.style.display = "none";
-	code.innerHTML = prettyPrintedJson;
+        pre.appendChild(code);
+        document.body.firstChild.style.display = "none";
+        code.innerHTML = prettyPrintedJson;
 
-	document.body.appendChild(pre);
+        document.body.appendChild(pre);
+    }
+} catch (ex) {
+    // do nothing
 }
