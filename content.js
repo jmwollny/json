c58ea6b39ce8ae1582e5d352ed196818f70e4cf1
Jonathan Wollny <jon.wollny@uk.ibm.com>
2020-05-13 23:15:49 +0100

    change extension

--- content.js
+++ content.js
@@ -1,56 +1,35 @@
-var library;
-var jsonObject;
-var prettyPrintedJson;
-var pre;
-var code;
-
-if (!library) {
-    var library = {};
+// http://jsfiddle.net/alanwsmith/EwT4E/
+function jsonPrettyHighlight(_jsonObj) {
+    
+    var json = JSON.stringify(_jsonObj, undefined, 2);
+    
+    json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+    json = json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
+        var cls = 'json-value-int';
+        if (/^"/.test(match)) {
+            if (/:$/.test(match)) {
+                cls = 'json-key';
+            } else {
+                cls = 'json-value-str';
+            }
+        }
+        return '<span class="' + cls + '">' + match + '</span>';
+    });
+    return json;    
 }
 
-// http://jsfiddle.net/unlsj/
-library.json = {
-    replacer: function(match, pIndent, pKey, pVal, pEnd) {
-        //var key = '<span class=json-key';
-        var key = '<span class=';
-        var val = '<span class=json-value>';
-        var str = '<span class=json-string>';
-        var r = pIndent || '';
-
-        if (pKey) {
-            var propName = pKey.replace(/[": ]/g, '');
-            if (propName === "name")
-                key += " json-name";
-            else if (propName === "id")
-                key += " json-id";
-            key += ">";
-            r = r + key + propName + '</span>: ';
-        }
-        if (pVal)
-            r = r + (pVal[0] == '"'
-                ? str
-                : val) + pVal + '</span>';
-        return r + (pEnd || '');
-    },
-    prettyPrint: function(obj) {
-        var jsonLine = /^( *)("[\w]+": )?("[^"]*"|[\w.+-]*)?([,[{])?$/mg;
-        return JSON.stringify(obj, null, 3).replace(/&/g, '&amp;').replace(/\\"/g, '&quot;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(jsonLine, library.json.replacer);
-    }
-};
 
 try {
     jsonObject = JSON.parse(document.body.innerText);
     if (jsonObject) {
-        var prettyPrintedJson = library.json.prettyPrint(jsonObject);
-        var pre = document.createElement("pre");
-        var code = document.createElement("code")
 
-        pre.appendChild(code);
+        var prettyPrintedJson = jsonPrettyHighlight(jsonObject);
+        var container = document.createElement("pre");
         document.body.firstChild.style.display = "none";
-        code.innerHTML = prettyPrintedJson;
 
-        document.body.appendChild(pre);
+        container.innerHTML = prettyPrintedJson;
+        document.body.appendChild(container);    
     }
 } catch (ex) {
-    // do nothing
+    // do nothing, the current page is not altered
 }
